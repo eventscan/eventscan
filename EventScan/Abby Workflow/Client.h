841f8496f17421247cdd6f045555966064f65710
akaisani <akaisani@purdue.edu>
2019-03-31 16:48:55 -0400

    Added ABBYHelper

---
+++ EventScan/Abby Workflow/Client.h
@@ -0,0 +1,36 @@
+#import <Foundation/Foundation.h>
+#import "HTTPOperation.h"
+#import "ProcessingParams.h"
+#import <UIKit/UIKit.h>
+#import "NSString+Base64.h"
+#import "NSData+Base64.h"
+
+
+@protocol ClientDelegate;
+
+@interface Client : NSObject<HTTPOperationAuthenticationDelegate> {
+
+}
+
+@property (strong, nonatomic) NSString* applicationID;
+@property (strong, nonatomic) NSString* password;
+
+
+@property (assign) id<ClientDelegate> delegate;
+
+- (id)initWithApplicationID:(NSString*)applicationID password:(NSString*)password;
+
+- (void)processImage:(UIImage*)image withParams:(ProcessingParams*)params;
+
+@end
+
+@protocol ClientDelegate <NSObject>
+
+@optional
+
+- (void)clientDidFinishUpload:(Client*)sender;
+- (void)clientDidFinishProcessing:(Client*)sender;
+- (void)client:(Client*)sender didFinishDownloadData:(NSData*)downloadedData;
+- (void)client:(Client*)sender didFailedWithError:(NSError*)error;
+
+@end
---
+++ EventScan/Abby Workflow/PhotoHelper.swift
@@ -0,0 +1,81 @@
+//
+//  File.swift
+//  WannaBeGram
+//
+//  Created by Abid Amirali on 2/17/19.
+//  Copyright © 2019 Abid Amirali. All rights reserved.
+//
+
+import UIKit
+
+class PhotoHelper: NSObject {
+    
+    var completionHandler: ((UIImage) -> Void)?
+    
+    func show(for viewController: UIViewController) {
+        let alertController = UIAlertController(title: "Select a method to take an image", message: "", preferredStyle: .actionSheet)
+        
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let cameraAction = UIAlertAction(title: "Camera", style: .default) { (action) in
+                self.presentImagePicker(from: viewController, of: .camera)
+            }
+            alertController.addAction(cameraAction)
+        }
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            
+            
+            let libraryAction = UIAlertAction(title: "Photo Library", style: .default) { (action) in
+                self.presentImagePicker(from: viewController, of: .photoLibrary)
+            }
+            alertController.addAction(libraryAction)
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (action) in
+//            viewController.dismiss(animated: true, completion: nil)
+        }
+        alertController.addAction(cancelAction)
+        
+        viewController.present(alertController, animated: true, completion: nil)
+    }
+    
+    
+    private func presentImagePicker(from viewcontroller: UIViewController, of type: UIImagePickerController.SourceType) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = type
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        
+        viewcontroller.present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+    
+    
+}
+
+
+extension PhotoHelper: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+//    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+//        guard let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage else {picker.dismiss(animated: true, completion: nil);return}
+//        completionHandler?(image)
+//        picker.dismiss(animated: true, completion: nil)
+//    }
+//
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+                guard let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage else {picker.dismiss(animated: true, completion: nil);return}
+                completionHandler?(image)
+                picker.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+}
