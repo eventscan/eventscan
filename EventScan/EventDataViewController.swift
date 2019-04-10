783702a2325ef939af841eb479352b918c8f4b2c
akaisani <akaisani@purdue.edu>
2019-04-10 18:16:39 -0400

    added eventdata view controller

---
+++ EventScan/EventDataViewController.swift
@@ -0,0 +1,49 @@
+//
+//  EventDataViewController.swift
+//  EventScan
+//
+//  Created by Abid Amirali on 4/8/19.
+//  Copyright © 2019 eventScan. All rights reserved.
+//
+
+import UIKit
+
+class EventDataViewController: UIViewController {
+
+    @IBOutlet weak var eventDataTableView: UITableView!
+    @IBOutlet weak var promptLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+
+extension EventDataViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+    
+    
+}
+
+extension EventDataViewController: UITableViewDelegate {}
