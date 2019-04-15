712360fb2316a980b3dc9e25c8a3080765f38577
akaisani <akaisani@purdue.edu>
2019-04-15 19:14:54 -0400

    added event struct to encapsulate parsed event information

---
+++ EventScan/Event.swift
@@ -0,0 +1,15 @@
+//
+//  Event.swift
+//  EventScan
+//
+//  Created by Abid Amirali on 4/15/19.
+//  Copyright © 2019 eventScan. All rights reserved.
+//
+
+import Foundation
+
+struct Event {
+    let name: String
+    let location: String
+    let date: String
+}
